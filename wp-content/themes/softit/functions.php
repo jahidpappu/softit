0063ec0e8ce28075f3b330855b0dad9918b135ab
jahidpappu <jahidpappublog@gmail.com>
2019-01-05 23:47:32 +0600

    নাাুুি

--- wp-content/themes/softit/functions.php
+++ wp-content/themes/softit/functions.php
@@ -1,3 +1,8 @@
 <?php
 
-register_nav_menu('main-menu','Main Menu');
+
+function first_theme_functions (){
+  add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+}
+add_action('after_setup_theme','first_theme_functions');
