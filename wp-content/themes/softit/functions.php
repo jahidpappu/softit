1f708ed3cc016122bd30d391001e7290bd80cd7e
jahidpappu <jahidpappublog@gmail.com>
2019-01-17 22:55:31 +0600

    new

--- wp-content/themes/softit/functions.php
+++ wp-content/themes/softit/functions.php
@@ -4,5 +4,12 @@
 function first_theme_functions (){
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
+  add_theme_support('woocommerce');
+
+  add_theme_support('custom_header');
+  add_theme_support('custom_background');
 }
 add_action('after_setup_theme','first_theme_functions');
+
+
+ 
--- wp-content/themes/softit/index.php
+++ wp-content/themes/softit/index.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+
+  <?php wp_head(); ?>
+
+</head>
+<body <?php body_class( ) ?> >
+  <div class="header-area">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/banner.png" alt="">
+    <img src="<?php header_image(); ?>" alt="">
+  </div>
 
  	<?php 
 	
@@ -7,3 +23,8 @@
     ));
     
  
+?>
+<?php wp_footer();?>  
+</body>
+</html>
+
